1ba0c9e4f26264bf735b0460a0367dd64d705a97
UlfaMaulidia <99638319+UlfaMaulidia@users.noreply.github.com>
2023-03-04 16:54:43 +0700

    Update index.php

--- mid/index.php
+++ mid/index.php
@@ -17,22 +17,23 @@ $result = mysqli_query($mysqli, "SELECT * FROM barang ORDER BY id DESC");
     <table width='80%' border=1>
  
     <tr>
-        <th>Id</th> 
         <th>Nama Barang</th> 
         <th>Kategori</th> 
         <th>Satuan</th>
         <th>Harga Satuan</th>
-        <th>Jumlah Satuan</th>
+        <th>Jumlah Barang</th>
+        <th>Total Harga (Diskon 20%)</th>
     </tr>
     <?php  
     while($user_data = mysqli_fetch_array($result)) {         
         echo "<tr>";
-        echo "<td>".$user_data['id']."</td>";
         echo "<td>".$user_data['nama_barang']."</td>";
         echo "<td>".$user_data['kategori']."</td>";    
         echo "<td>".$user_data['satuan']."</td>";    
         echo "<td>".$user_data['harga_satuan']."</td>";    
-        echo "<td>".$user_data['jumlah_satuan']."</td>"; 
+        echo "<td>".$user_data['jumlah_satuan']."</td>";
+        echo "<td>".$user_data['total']."</td>";
+
     }
     ?>
     </table>
